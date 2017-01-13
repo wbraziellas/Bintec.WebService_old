3bc6d7707c71cd09fb1c8e3a0508e160fdee0baa
wbraziellas <wbraziellas@hotmail.com>
2017-01-13 15:15:58 +0000

    First commit

---
+++ Bintec.WebService/Bintec.WebService.Domain/ConexaoMySql/Base/ConexaoBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bintec.WebService.Repository.Base
+{
+    public abstract class ConexaoBase
+    {
+        protected string _strConnection = string.Empty;
+
+        public abstract bool Conectar();
+        public abstract bool Desconectar();
+    }
+}
---
+++ Bintec.WebService/Bintec.WebService.Domain/ConexaoMySql/ConexaoMySql.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bintec.WebService.Repository.Base;
+using MySql.Data.MySqlClient;
+
+namespace Bintec.WebService.Repository.ConexaoMySql
+{
+    public class ConexaoMySql : ConexaoBase
+    {
+        public override bool Conectar()
+        {
+
+            _strConnection = "";
+
+            MySqlConnection _conexaoMySQL = new MySqlConnection(_strConnection);
+
+
+            throw new NotImplementedException();
+        }
+
+        public override bool Desconectar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
